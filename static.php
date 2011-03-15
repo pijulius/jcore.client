0e892b99b4a7611afb542778b090a0034c69dc8c
Istvan Petres <me@pijulius.com>
2011-03-15 15:08:49 +0200

    Changed license to GPL, LGPL and MPL

--- static.php
+++ static.php
@@ -1,4 +1,14 @@
 <?php 
+
+/***************************************************************************
+ *            static.php
+ *
+ *  Jul 05, 07:00:00 2009
+ *  Copyright  2009  Istvan Petres (aka P.I.Julius)
+ *  me@pijulius.com
+ *  For licensing, see LICENSE or http://jcore.net/license
+ ****************************************************************************/
+ 
 	include_once('jcore.inc.php');
 	include_once(JCORE_PATH.'static.php');
 ?>
